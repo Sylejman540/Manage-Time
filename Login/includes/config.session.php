7f56c45cd229ed7ba3edb1764bc26adb4e293be7
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-05-03 19:42:39 +0200

    Update config.session.php

--- Login/includes/config.session.php
+++ Login/includes/config.session.php
@@ -1,23 +1,2 @@
 <?php
-
-if(isset($_SESSION["user_id"])){
-    if (!isset($_SESSION["last_regeneration"])) {
-             regenerate_session_id();
-    } else {
-        $interval = 60 * 30;                       // 30 minutes
-        if (time() - $_SESSION["last_regeneration"] >= $interval) {
-        regenerate_session_id();
-    }
-    }
-}
-
-function regenerate_session_id_loggedin(){
-         session_regenerate_id(true);
-    
-         $userId =  $_SESSION["user_id"];
-         $newSessionId = session_create_id();
-           $session_id = $newSessionId . "_" . $result["id"];
-            session_id($session_id);
-    
-         $_SESSION["last_regeneration"] = time();
-    } 
+session_start();
