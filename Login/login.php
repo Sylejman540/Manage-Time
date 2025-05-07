75bcae48ef278e3659c4fb6a560838aa787fc5ef
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-05-07 19:06:02 +0200

    Update login.php

--- Login/login.php
+++ Login/login.php
@@ -1,5 +1,6 @@
 <?php
-    require_once 'includes/config.session.php';
+   require_once 'includes/config.session.inc.php';
+   require_once 'includes/login.view.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -17,37 +18,20 @@
         Log <span class="text-blue-500">In</span>
     </h1>
 
-    <!-- Login Form -->
-    <form class="flex flex-col items-center mt-20" action="includes/login.inc.php" method="POST">
-        <!-- Email Input -->
-        <input type="email" name="email" placeholder="Email" required 
-               class="w-[260px] p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5869FF]">
-
-        <!-- Password Input -->
-        <input type="password" name="password" placeholder="Password" required 
-               class="w-[260px] p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5869FF]"> 
-
-        <!-- Submit Button -->
-        <button type="submit" name="submit" 
-                class="bg-blue-500 w-[260px] text-white font-bold p-2 rounded-lg mb-5 hover:bg-blue-800">
-            Login
-        </button>
+    <?php
+         check_login_errors();
+    ?>  
+    <!-- LOGIN -->
+    <form class="flex flex-col items-center mt-20" action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username" class="w-[260px] p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5869FF]">
+        <input type="text" name="password" placeholder="Password" class="w-[260px] p-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5869FF]"> 
+        <button type="submit" name="submit" class="bg-blue-500 w-[260px] text-white font-bold p-2 rounded-lg mb-5 hover:bg-blue-800">Login</button>
     </form>
 
     <p class="text-center mt-5 text-gray-600">
         Don't have an account?
         <a href="/Manage Time/Signup/index.php" class="text-blue-500 font-semibold hover:underline">Signup</a>
     </p>
-
-    <!-- Display Login Errors -->
-    <?php if (isset($_SESSION['login_error'])): ?>
-        <p class="text-red-500 text-center mt-5">
-            <?php 
-                echo $_SESSION['login_error']; 
-                unset($_SESSION['login_error']); 
-            ?>
-        </p>
-    <?php endif; ?>
 
 </body>
 </html>
