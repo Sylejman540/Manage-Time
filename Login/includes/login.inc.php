5bcd45b839d0969615e6514c315db1764bd2910c
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-05-08 23:21:37 +0200

    Update login.inc.php

--- Login/includes/login.inc.php
+++ Login/includes/login.inc.php
@@ -1,55 +1,27 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] === "POST"){
-    
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    
-    try{
+    $localhost = "localhost";
+    $dbname = "myfirstdatabase";
+    $dbuser =  "root";
+    $dbpass = "";
 
-        require_once 'dbh.inc.php';
-        require_once 'login.model.inc.php';
-        require_once 'login_contr.inc.php';
+    $conn = new mysqli($localhost, $dbuser, $dbpass, $dbname);
 
-        // Errors handler
-        $errors = [];
-
-        if(is_input_empty($username, $password)){
-            $errors["empty_input"] = "Fill in all fields";
-        }
-
-        $result = get_user($pdo, $username);
-
-        if(is_username_wrong($result)){
-            $errors["username_not_found"] = "Username not found";
-        }
-
-        if (!is_username_wrong($result) && password_verify($password, $result["password"])) {
-            $errors["wrong_password"] = "Wrong password";
-        }
-        
-
-        require_once 'config.session.inc.php';
-
-        if($errors){
-            $_SESSION["login_error"] = $errors;
-            
-            header("Location: ../index.php?error=loginfailed");
-            die();
-        }
-
-        header("Location: /Manage Time/main-page/main.php?success=login");
-        $pdo = null;
-        $statement = null;
-
-        die();
-        
-    } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+    if($conn->connect_error){
+        die("Connection failed:" . $conn->connect_erorr);
     }
 
-}else{
-    header("Location: /Manage Time/main-page/main.php?success=login");
-    die();
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result= $conn->query($sql);
+    if($result->num_rows == 1){
+        header("Location: /Manage Time/main-page/main.php?success=Login successful");
+        exit();
+    }else{
+        header("Location: /Manage Time/Login/login.php?error=Invalid username or password");
+        exit();
+    }
 }
